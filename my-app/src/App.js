9cf35cdbcb914ae945641374059ebf29225afbbb
Mikhail Marchenko <marcheys@yandex.ru>
2024-12-08 17:40:46 +0300

    App createElement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,41 @@
-import logo from './logo.svg'; //императивный
-import './App.css'; //императивный
-//декларативный
+import logo from './logo.svg';
+import './App.css';
+import { createElement } from 'react';
+
 export const App = () => {
-	//императивный
-	return (
-		//императивный
-		//все что внутри return деклоративный
-		<div className="App">
-			<header className="App-header">
-				<img
-					src={logo}
-					className="App-logo"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement(
+				'img',
+				{
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				},
+				null,
+			),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', null, new Date().getFullYear()),
+		),
 	);
 };
